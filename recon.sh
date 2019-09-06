cd48d5b39fcf3c0ee8e91cb987b8b024523d9fd3
im-gr00t <54957427+im-gr00t@users.noreply.github.com>
2019-09-06 16:00:37 -0500

    Update recon.sh

--- recon.sh
+++ recon.sh
@@ -4,13 +4,13 @@
 # Check if user is supplying an argument
 if [ -z "$1" ]
 then
-        echo "Usage: $0 --help";
+        echo "\e[31m;Usage: $0 --help\e[0m;";
         exit 2;
 
 # Check for valid help flag
 elif [ "$1" == "-h" -o "$1" == "--help" ]
 then
-        echo "Usage: $0 [options] <directory>";
+        echo "\e[31m;Usage: $0 [options] <directory>\e[0m;";
         echo "Options:";
         echo -e "\t-h, --help\tDisplay this help message";
         echo -e "\t-d, --domain\tSpecifies desired directory to save all tools in (will be created if it doesn't exist)";
@@ -25,7 +25,7 @@ then
         # Check if "--domain" has valid argument
         if [ -z "$2" ]
         then
-                echo "Usage: $0 --help"
+                echo "\e[31m;Usage: $0 --help\e[0m;"
                 exit 2;
         fi
         
@@ -34,33 +34,38 @@ then
         
         # Display target being enumerated
         echo -e "\e[31m[+]\e[0m \e[36mTarget:\e[0m \e[93m${DOMAIN}\e[0m"
+        sleep(3)
 
         # Create the directory to store output
-        mkdir -p ${PATH}
+        mkdir -p ${DIR}
 
         # Run subfinder
         echo -e "\e[31m[+]\e[0m \e[36mStarting subfinder: \e[0m\e[93mforeground\e[0m"
-        subfinder -d ${DOMAIN} -o ${PATH}/subfinder_output.txt
+        subfinder -d ${DOMAIN} -o ${DIR}/subfinder_output.txt
+        sleep(3)
 
         # Run sublister
         echo -e "\e[31m[+]\e[0m \e[36mStarting sublist3r: \e[0m\e[93foreground\e[0m"
-        sublist3r -d ${DOMAIN} -o ${PATH}/sublist3r_output.txt
-
+        sublist3r -d ${DOMAIN} -o ${DIR}/sublist3r_output.txt
+        sleep(3)
+        
         # Run assetfinder
         echo -e "\e[31m[+]\e[0m \e[36mStarting assetfinder: \e[0m\e[93mforeground\e[0m"
-        assetfinder -subs-only ${DOMAIN} | tee -a ${PATH}/assetfinder_output.txt
+        assetfinder -subs-only ${DOMAIN} | tee -a ${DIR}/assetfinder_output.txt
+        sleep(3)
 
         # Run amass
         echo -e "\e[31m[+]\e[0m \e[36mStarting amass: \e[0m\e[93mforeground\e[0m"
-        amass enum -o ${PATH}/amass_output.txt -d ${DOMAIN}
+        amass enum -o ${DIR}/amass_output.txt -d ${DOMAIN}
+        sleep(3)
         
         # Cat and sort -u all domains into a single file called "all_domains_sorted.txt"
-        cd $PATH;
+        cd $DIR;
         cat *.txt >> all_domains.txt;
         sort -u all_domains.txt >> all_domains_sorted.txt;
         rm all_domains.txt;
         echo -e "\e[31m[+]\e[0m \e[36mDone\e[0m\e[93m...\e[0m"
 else
-        echo "Usage: $0 --help"
+        echo "\e[31m;Usage: $0 --help\e[0m;"
         exit 2
 fi
