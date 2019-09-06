883b7e66dedb4eadaabc31ca309dde4b39617be6
im-gr00t <54957427+im-gr00t@users.noreply.github.com>
2019-09-06 02:10:27 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,76 +1,49 @@
 #!/bin/bash
 
-# Check if user is supplying an argument 
+# TODO: implement `getopt'
+# Check if user is supplying an argument
 if [ -z "$1" ]
 then
-	echo "Usage: $0 --help";
-	exit 2;
+        echo "Usage: $0 --help";
+        exit 2;
 
 # Check for valid help flag
 elif [ "$1" == "-h" -o "$1" == "--help" ]
 then
-	echo "Usage: $0 [options] <directory>";
-	echo "Options:";
-	echo -e "\t-h, --help\tDisplay this help message";
-	echo -e "\t-d, --directory\tSpecifies desired directory to save all tools in (will be created if it doesn't exist)";
-	echo -e "Example:";
-	echo -e "\t$0 --directory /opt";
-	exit 2;
+        echo "Usage: $0 [options] <directory>";
+        echo "Options:";
+        echo -e "\t-h, --help\tDisplay this help message";
+        echo -e "\t-d, --domain\tSpecifies desired directory to save all tools in (will be created if it doesn't exist)";
+        echo -e "Example:";
+        echo -e "\t$0 --domain example.com";
+        exit 2;
 fi
 
-DIR=$2
-# Check for valid directory
-if [ "$1" == "-d" -o "$1" == "--directory" -a -d ${DIR} ]
+DOMAIN=$2
+# Check for valid command line option
+if [ "$1" == "-d" -o "$1" == "--domain" ]
 then
-	echo "[+] Using directory to save all tools in: ${DIR}";
-	# Create the directory
-	mkdir ${DIR};
+        # Display target being enumerated
+        echo -e "\e[31m[+]\e[0m \e[36mTarget:\e[0m \e[93m${DOMAIN}\e[0m"
 
-	# cd into the directory
-	cd ${DIR};
+        # Create the directory to store output
+        mkdir -p ~/Documents/${DOMAIN}/RECON/DNS/
 
-	# Update system, download required dependencies and packages
-	echo "[+] Updating system"
-	sudo apt update && sudo apt install golang git python3.7;
+        # Run subfinder
+        echo -e "\e[31m[+]\e[0m \e[36mStarting subfinder: \e[0m\e[93mforeground\e[0m"
+        subfinder -d ${DOMAIN} -o ~/Documents/${DOMAIN}/RECON/DNS/subfinder_output.txt
 
-	# Get subfinder
-	# Credit: https://github.com/subfinder
-	# Repo: https://github.com/subfinder/subfinder
-	echo "[+] Downloading subfinder";
-	go get github.com/subfinder/subfinder;
-	sudo mv -f ~/go ~/subfinder;
-	sudo mv -f ~/subfinder .;
+        # Run sublister
+        echo -e "\e[31m[+]\e[0m \e[36mStarting sublist3r: \e[0m\e[93foreground\e[0m"
+        sublist3r -d ${DOMAIN} -o ~/Documents/${DOMAIN}/RECON/DNS/sublist3r_output.txt
 
-	# Create soft link
-	sudo ln -sf $(pwd)/subfinder/bin/subfinder /usr/bin/subfinder;
+        # Run assetfinder
+        echo -e "\e[31m[+]\e[0m \e[36mStarting assetfinder: \e[0m\e[93mforeground\e[0m"
+        assetfinder -subs-only ${DOMAIN} | tee -a ~/Documents/${DOMAIN}/RECON/DNS/assetfinder_output.txt
 
-	# Get sublist3r
-	# Credit to Auther: aboul3la and TheRook
-	# Repo: https://github.com/aboul3la/Sublist3r
-	echo "[+] Downloading sublist3r";
-	git clone https://github.com/aboul3la/Sublist3r.git;
-	cd Sublist3r/;
-	pip install -r requirements.txt;
-	cd ../;
-
-	# Create soft link
-	sudo ln -sf $(pwd)/Sublist3r/sublist3r.py /usr/bin/sublist3r;
-
-	# Get assetfinder
-	# Credit to Author: tomnomnom
-	# Repo: https://github.com/tomnomnom/assetfinder
-	echo "[+] Downloading assetfinder";
-	go get -u github.com/tomnomnom/assetfinder;
-	sudo mv -f ~/go ~/assetfinder;
-	sudo mv -f ~/assetfinder .;
-
-	# Create soft link
-	sudo ln -sf $(pwd)/assetfinder/bin/assetfinder /usr/bin/assetfinder;
-
-	# Finished
-	echo "[+] Done...";
-	exit 0;
+        # Run amass
+        echo -e "\e[31m[+]\e[0m \e[36mStarting amass: \e[0m\e[93mforeground\e[0m"
+        amass enum -o ~/Documents/${DOMAIN}/RECON/DNS/amass_output.txt -d ${DOMAIN}
 else
-	echo "Usage: $0 --help";
-	exit 2;
+        echo "Usage: $0 --help"
 fi
